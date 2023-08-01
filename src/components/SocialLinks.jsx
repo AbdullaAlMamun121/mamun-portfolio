fcbbe42baa497e845fbf27cd51b5ac270d98e5d3
AbdullaAlMamun121 <hmdabdulla121@gmail.com>
2023-08-01 19:51:07 +0600

    resume replace

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/mamunResume.pdf",
+      href: "/Web Developer Resume of  Mamun.pdf",
       style: "rounded-br-md",
       download: true
     }
